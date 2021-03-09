0b27de4c0bf325d1649a7a88273f7a75e1813e0d
AntonTuzov <antontuzovf@gmail.com>
2021-03-09 14:11:15 +0300

    Initial Commit

---
+++ SimpleMovieSearcher/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SimpleMovieSearcher
+//
+//  Created by turbo on 09.03.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
